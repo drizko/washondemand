68bde0dc9704f03e4851bafe7d18b520776a03c4
Kasra Jahani <Kasra.Jahani@gmail.com>
2016-03-11 14:49:07 -0800

    [feature] Implements customer authentication on server and db

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -2,4 +2,7 @@ var customerController = require('../controllers/customerController.js');
 
 module.exports = function(app) {
 
+	app.post('/signin', customerController.signin);
+	app.post('/signup', customerController.signup);
+	// app.get('/', customerController.checkAuth);
 };
