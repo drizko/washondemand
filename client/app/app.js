93f27d47e8bb5b782e159df43dc2c9509581b9b9
Kyle Kodani <poolioman@sbcglobal.net>
2016-03-11 10:47:20 -0800

    [feature]Adds more routes for profile pages

--- client/app/app.js
+++ client/app/app.js
@@ -7,25 +7,37 @@ angular.module('WashOnDemand', [
 		.state('customerSignin', {
       url: '/customerSignin',
       templateUrl: 'app/auth/customerAuth/signin.html',
-      controller: 'custSI',
+      controller: 'custSICtrl',
       authenticate: false
     })
 		.state('customerSignup', {
       url: '/customerSignup',
       templateUrl: 'app/auth/customerAuth/signup.html',
-      controller: 'custSU',
+      controller: 'custSUCtrl',
       authenticate: false
     })
 		.state('providerSignin', {
       url: '/providerSignin',
       templateUrl: 'app/auth/providerAuth/signin.html',
-      controller: 'provSI',
+      controller: 'provSICtrl',
       authenticate: false
     })
     .state('providerSignup', {
       url: '/providerSignup',
       templateUrl: 'app/auth/providerAuth/signup.html',
-      controller: 'provSU',
+      controller: 'provSUCtrl',
       authenticate: false
+    })
+    .state('customerView', {
+      url: '/customerProfile',
+      templateUrl: 'app/customer/customer.html',
+      controller: 'customer',
+      authenticate: true
+    })
+    .state('providerView', {
+      url: '/providerProfile',
+      templateUrl: 'app/provider/provider.html',
+      controller: 'provider',
+      authenticate: true
     });
 });
