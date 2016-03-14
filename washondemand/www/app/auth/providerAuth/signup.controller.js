9f66313718c2115fcb6f0e14f6d51790ef546918
Kyle Kodani <poolioman@sbcglobal.net>
2016-03-14 11:24:30 -0700

    [chore]ports signin-up code to ionic mobile framework

--- washondemand/www/app/auth/providerAuth/signup.controller.js
+++ washondemand/www/app/auth/providerAuth/signup.controller.js
@@ -1,5 +1,26 @@
-angular.module('wod.provSU', []).controller('provSUCtrl', provSUCtrlCtrl);
+angular.module('wod.provSU', []).controller('provSUCtrl', provSUCtrl);
 
-function provSUCtrlCtrl() {
-  //
-};
+function provSUCtrl(authFactory, $window, $location) {
+  var vm = this;
+  vm.provider = {
+    companyName: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  vm.signup = function() {
+    console.log(vm.provider);
+    //call factory
+    authFactory.provSignup(vm.provider)
+    .then(function(token) {
+      authFactory.clearForm(vm.provider);
+      $window.localStorage.setItem('com.wod', token);
+      $location.path('/providerProfile');
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  };
+}
--- washondemand/www/app/routes.js
+++ washondemand/www/app/routes.js
@@ -17,25 +17,25 @@ angular.module('wod.routes', [])
 		.state('customerSignin', {
       url: '/customerSignin',
       templateUrl: 'app/auth/customerAuth/signin.html',
-      controller: 'custSICtrl',
+      controller: 'custSICtrl as csi',
       authenticate: false
     })
 		.state('customerSignup', {
       url: '/customerSignup',
       templateUrl: 'app/auth/customerAuth/signup.html',
-      controller: 'custSUCtrl',
+      controller: 'custSUCtrl as csu',
       authenticate: false
     })
 		.state('providerSignin', {
       url: '/providerSignin',
       templateUrl: 'app/auth/providerAuth/signin.html',
-      controller: 'provSICtrl',
+      controller: 'provSICtrl as psi',
       authenticate: false
     })
     .state('providerSignup', {
       url: '/providerSignup',
       templateUrl: 'app/auth/providerAuth/signup.html',
-      controller: 'provSUCtrl',
+      controller: 'provSUCtrl as psu',
       authenticate: false
     })
     .state('customerView', {
@@ -52,4 +52,31 @@ angular.module('wod.routes', [])
     });
 
   $urlRouterProvider.otherwise('/');
+  $httpProvider.interceptors.push('AttachTokens');
+})
+
+.factory('AttachTokens', function($window) {
+
+  var attach = {
+    request: function(object) {
+      var jwt = $window.localStorage.getItem('com.wod');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+
+.run(function($rootScope, $state, authFactory) {
+
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    if (toState.authenticate && !authFactory.isAuth()) {
+      // User isn’t authenticated
+      $state.transitionTo('home');
+      event.preventDefault();
+    }
+  });
 });
