d13d1f1abdb9b32d42146e1a7423b30365ca1242
Danny Rizko <drizko07@gmail.com>
2016-03-15 11:08:53 -0700

    Delete config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  dbUser: 'admin',
-  dbPass: '1234',
-  MAPS_API_KEY: 'AIzaSyDilAiZNyLBDx7A-PKD8z7VLUnLYs0s1AU',
-  tokenSecret: 'secret',
-  mongoUrl: 'mongodb://admin:1234@ds011389.mlab.com:11389/washondemand',
-  SALT_FACTOR: 10
-}
