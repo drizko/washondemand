f93facdf8b65d5c95d4daeff5f1e55f2f1e97af0
bobbychong <bobbychong@gmail.com>
2016-03-15 17:03:54 -0700

    [feature]implement provider view/factory

--- washondemand/www/app/customerView/customer.factory.js
+++ washondemand/www/app/customerView/customer.factory.js
@@ -4,11 +4,23 @@ angular.module('wod.customerFactory', [])
 function customerFactory($http, $window, $location) {
 
   function sendRequest(details) {
-    console.log("From customerFactory!!!!!!!!!!!!!");
+    console.log('From customerFactory!!!!!!!!!!!!!');
     return $http({
       method: 'POST',
       url: 'http://washondemand.us-west-2.elasticbeanstalk.com/api/request/create-request',
       data: details
+    })
+    .then(function(results) {
+      return results.data;
+    });
+  };
+
+  function getProviders(userLoc) {
+    console.log('Getting available providers!!!!');
+    return $http({
+      method: 'POST',
+      url: 'http://washondemand.us-west-2.elasticbeanstalk.com/api/request/get-providers',
+      data: userLoc
     })
     .then(function(results) {
       return results.data;
