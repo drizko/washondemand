fd0275060f6368d9cb8515b8fc880c291e1259c7
Danny Rizko <drizko07@gmail.com>
2016-03-18 15:07:28 -0700

    Removes root server file

--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require('./server/server.js');
-
-var port = process.env.PORT || 8000;
-
-app.listen(port);
-
-console.log('Express server listening on %d in %s mode', port, app.settings.env);
