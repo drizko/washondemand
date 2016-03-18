6ea30c571b051e20d7f1150ab3246d9ed19b59ba
Kasra Jahani <Kasra.Jahani@gmail.com>
2016-03-18 12:23:37 -0700

    [update] Adds a missing route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -22,6 +22,7 @@ module.exports = function(app, express) {
   var customerRouter = express.Router();
   var providerRouter = express.Router();
   var requestRouter = express.Router();
+  var historyRouter = express.Router();
 
   //
 
