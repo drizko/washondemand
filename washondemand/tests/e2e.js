70fff945d91c2af43ce764627332a92b3cd2f522
Danny Rizko <drizko07@gmail.com>
2016-03-21 11:31:50 -0700

    Begins to implements request a wash test.

--- washondemand/tests/e2e.js
+++ washondemand/tests/e2e.js
@@ -32,3 +32,45 @@ describe('Clicking on the signin button ', function() {
   //   // TODO: test unsuccessful login
   // });
 });
+describe('Requesting Wash', function() {
+
+  var requestWashButton, selectWashType, selectVehicleType,
+      email, password;
+
+
+  it('should allow user to request a wash', function() {
+    browser.get('/#/customerSignin');
+
+    email = element(by.model('csi.customer.email'));
+    password = element(by.model('csi.customer.password'));
+    signinButton = element(by.buttonText('Sign In'));
+
+    email.sendKeys('cust1@test.com');
+    password.sendKeys('123');
+
+    signinButton.click().then(function() {
+      expect(browser.getLocationAbsUrl()).toMatch('/nav1/customerProfile');
+    });
+
+    requestWashButton = element(by.buttonText('Request A Wash'));
+    selectWashType = element(by.buttonText('Basic'));
+    selectVehicleType = element(by.buttonText('Car'));
+
+    selectWashType.click()
+
+    selectVehicleType.click()
+
+    browser.sleep(5000);
+
+    requestWashButton.click()
+      .then(function(){
+        // Need to fill in more information here.
+      })
+
+    browser.sleep(2000);
+  });
+
+  // it('should display a popup for an unsuccessful login', function() {
+  //   // TODO: test unsuccessful login
+  // });
+});
