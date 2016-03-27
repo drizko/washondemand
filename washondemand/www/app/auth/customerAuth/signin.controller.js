9b00cbf4a5aed4cb50d3a5eb2e0d5965688a1adc
Danny Rizko <drizko07@gmail.com>
2016-03-26 18:10:53 -0700

    Implements persistent login

--- washondemand/www/app/auth/customerAuth/signin.controller.js
+++ washondemand/www/app/auth/customerAuth/signin.controller.js
@@ -1,8 +1,8 @@
 angular.module('wod.custSI', []).controller('custSICtrl', custSICtrl);
 
-custSICtrl.$injesct = ['authFactory', 'locFactory'];
+custSICtrl.$injesct = ['authFactory', '$scope', '$cordovaFile', '$cordovaDevice', 'locFactory', '$window', '$state'];
 
-function custSICtrl(authFactory, locFactory) {
+function custSICtrl(authFactory, $scope, $cordovaFile, $cordovaDevice, locFactory, $window, $state) {
   var vm = this;
   vm.customer = {
     email: '',
@@ -10,8 +10,25 @@ function custSICtrl(authFactory, locFactory) {
     error: ''
   };
 
+  if (ionic.Platform.isIOS()) {
+    $cordovaFile.readAsText(cordova.file.dataDirectory, 'cust')
+      .then( function(result) {
+        if(result === 'true'){
+          $state.go('customernav.customer');
+        } else {
+          $state.go('customerSignin');
+        }
+      });
+  }
+
   vm.signin = function() {
-    //call factory
+    if (ionic.Platform.isIOS()) {
+      $cordovaFile.writeFile(cordova.file.dataDirectory, 'cust', 'true', true)
+        .then( function(result) {
+          console.log("Wrote file");
+        });
+    };
+
     var emailCopy = vm.customer.email;
     authFactory.handleAuth(vm.customer, 'customer', 'signin');
     locFactory.getLoc('customer', emailCopy).then(locFactory.sendLocToServer);
--- washondemand/www/app/auth/providerAuth/signin.controller.js
+++ washondemand/www/app/auth/providerAuth/signin.controller.js
@@ -1,8 +1,8 @@
 angular.module('wod.provSI', []).controller('provSICtrl', provSICtrl);
 
-provSICtrl.$inject = ['authFactory', 'locFactory'];
+provSICtrl.$inject = ['authFactory', '$cordovaFile', 'locFactory', '$window', '$state'];
 
-function provSICtrl(authFactory, locFactory) {
+function provSICtrl(authFactory, $cordovaFile, locFactory, $window, $state) {
   var vm = this;
   vm.provider = {
     email: '',
@@ -10,8 +10,26 @@ function provSICtrl(authFactory, locFactory) {
     error: ''
   };
 
+  if (ionic.Platform.isIOS()) {
+    $cordovaFile.readAsText(cordova.file.dataDirectory, 'prov')
+      .then( function(result) {
+        if(result === 'true'){
+          $state.go('providernav.provider');
+        } else {
+          $state.go('providerSignin');
+        }
+      });
+  }
+
   vm.signin = function() {
-    //call factory
+
+    if (ionic.Platform.isIOS()) {
+      $cordovaFile.writeFile(cordova.file.dataDirectory, 'prov', 'true', true)
+        .then( function(result) {
+          console.log("Wrote file");
+        });
+    };
+
     var emailCopy = vm.provider.email;
     authFactory.handleAuth(vm.provider, 'provider', 'signin');
     locFactory.getLoc('provider', emailCopy).then(locFactory.sendLocToServer);
