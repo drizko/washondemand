c389750cccdeace644accf1fc9011d7903b00800
Danny Rizko <drizko07@gmail.com>
2016-03-28 15:58:51 -0700

    [update]Fixes persistent login on app open

--- washondemand/www/app/home/home.controller.js
+++ washondemand/www/app/home/home.controller.js
@@ -1,9 +1,58 @@
 angular.module('wod.home', []).controller('homeCtrl', homeCtrl);
 
-homeCtrl.$inject = ['authFactory'];
+homeCtrl.$inject = ['authFactory', '$cordovaDevice', '$cordovaFile', '$state', '$window'];
 
-function homeCtrl(authFactory) {
+function homeCtrl(authFactory, $cordovaDevice, $cordovaFile, $state, $window) {
   var vm = this;
+
+  function init(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+  }
+
+  function onDeviceReady(){
+    if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
+      $cordovaFile.checkFile(cordova.file.dataDirectory, 'cust')
+        .then(function(result){
+          console.log("Inside somewhere", result);
+          $cordovaFile.readAsText(cordova.file.dataDirectory, 'cust')
+          .then(function(result) {
+            if(result === 'true'){
+              $cordovaFile.readAsText(cordova.file.dataDirectory, 'com.wod')
+              .then(function(result){
+                $window.localStorage['com.wod'] = result;
+                $state.go('customernav.customer');
+              })
+            } else {
+              console.log("Not result from cust");
+            }
+          })
+        }, function(error){
+          console.log("Error!!!!!!!!!!!!", error);
+        });
+
+      $cordovaFile.checkFile(cordova.file.dataDirectory, 'prov')
+        .then(function(result){
+          console.log("Inside somewhere", result);
+          $cordovaFile.readAsText(cordova.file.dataDirectory, 'prov')
+          .then(function(result) {
+            if(result === 'true'){
+              $cordovaFile.readAsText(cordova.file.dataDirectory, 'com.wod')
+              .then(function(result){
+                $window.localStorage['com.wod'] = result;
+                $state.go('providernav.provider');
+              })
+            } else {
+              console.log("Not result");
+            }
+          })
+        }, function(error){
+          console.log("Error!!!!!!!!!!!!", error);
+        });
+    }
+  }
+
+  init();
+
 
   vm.logoutTest = function() {
     authFactory.signout();
