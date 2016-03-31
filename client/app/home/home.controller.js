0aa5ee82b696c2b8d1bd308a3bce3f4660763e2c
bobbychong <bobbychong@gmail.com>
2016-03-31 12:14:29 -0700

    [feature]implement landing page

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,5 +1,8 @@
 angular.module('wod.home', []).controller('homeCtrl', homeCtrl);
 
 function homeCtrl() {
-  //
+  //$(document).ready(function () {
+  $('.navbar-nav li a').click(function(event) {
+    $('.navbar-collapse').collapse('hide');
+  });
 }
